24e936c10ed02af33de7ec835ec44bada47eb51e
Imre Kli. <imre.koli@tktk.ee>
2023-11-03 16:48:27 +0200

    add week 2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutajal valida tema sugu (m/f)
+//rakendus küsib kasutajal sisestada tema perekonnanime
+//lähtudes kasutaja valikust, rakendus tervitab kasutajalt järgmiselt:
+//"Welcome, Mr. [kasutaja perekonnanimi] / "Welcome, Ms. [kasutaja perekonnanimi]"
+
+using System;
+
+Console.WriteLine("Please, select your gender (m/f):");
+
+char userGender = Char.Parse(Console.ReadLine()); //loeb konsoolit maha andmeid string (sõna formaadis)
+                                                  //
+Console.WriteLine("Please, enter your last name");
+string userLastName = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
---
+++ UserAgeValidation1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutajalt sisestada tema vanus
+//kui kasutaja vanus on väiksem kui 13, siis konsoolis kuvatakse:
+//"You are too young to use Instagram"
+//muul juhul
+//konsoolis kuvatakse "Welcome to Instagram"
+
+using System.Runtime.Intrinsics.X86;
+
+Console.WriteLine("Enter your age:");
+
+int userAge = Int32.Parse(Console.ReadLine ()); //"13" - heap, 13 - stack
+
+if (userAge >= 13)
+{
+    Console.WriteLine("Welcome to Instagram!");
+}
+else
+{
+    Console.WriteLine("You are too young to use Instagram.");
+}
+
+
